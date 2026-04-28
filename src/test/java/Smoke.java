eb43d919b40c99c4f474f538ac7ce526618084e0
abokhalifa <ahmed.abokhalifa@gmail.com>
2026-04-28 11:47:13 +0300

    Remove unneeded lines.

--- src/test/java/Smoke.java
+++ src/test/java/Smoke.java
@@ -14,10 +14,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class Smoke {
     private WebDriver driver;
