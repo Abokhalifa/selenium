4babe6e58d33a828d75c8d408fb83ea9aed6f9b5
abokhalifa <ahmed.abokhalifa@gmail.com>
2026-04-30 20:32:46 +0300

    Add tests with different data sources.

--- src/test/java/Smoke.java
+++ src/test/java/Smoke.java
@@ -1,28 +1,28 @@
-import Pages.HomePage;
 import Pages.LandingPage;
 import Pages.LoginForm;
 import Pages.PersonalPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import utils.DataAdapter;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class Smoke {
     private WebDriver driver;
+    private String testCaseName;
+    private String username;
+    private String password;
+    private LandingPage landingPage;
+    private LoginForm loginForm;
+    private PersonalPage personalPage;
+
 
     @BeforeTest
     @Parameters({"browser","env"})
-    public void setUp(String browser, String env){
+    public void setUp(String browser, String env) throws IOException {
         System.out.println("Running tests in browser: "+browser);
         System.out.println("Running tests in environment: "+env);
         driver = new ChromeDriver();
@@ -35,15 +35,62 @@ public class Smoke {
 
     @Parameters({"username","password"})
     @Test
-    public void loginValidUser(String username, String password)  {
-        LandingPage landingPage = new LandingPage(driver);
-        LoginForm loginForm = landingPage.openLoginForm();
-        PersonalPage personalPage = loginForm.loginValidUser(username,password);
+    public void loginValidUserFromTestNg(String username_testng, String password_testng) throws IOException {
+        testCaseName = DataAdapter.readDataFromExcelFile()[0];
+        username = username_testng;
+        password = password_testng;
+        DataAdapter.readDataFromTextFile();
+        landingPage = new LandingPage(driver);
+        loginForm = landingPage.openLoginForm();
+        personalPage = loginForm.loginValidUser(username_testng,password_testng);
+        Assert.assertEquals(personalPage.getWelcomeText(),"Welcome "+username_testng);
+    }
+
+    @Test
+    public void loginValidUserFromExcel() throws IOException {
+        testCaseName = DataAdapter.readDataFromExcelFile()[0];
+        username = DataAdapter.readDataFromExcelFile()[1];
+        password = DataAdapter.readDataFromExcelFile()[2];
+        DataAdapter.readDataFromTextFile();
+        landingPage = new LandingPage(driver);
+        loginForm = landingPage.openLoginForm();
+        personalPage = loginForm.loginValidUser(username, password);
         Assert.assertEquals(personalPage.getWelcomeText(),"Welcome "+username);
+    }
+
+    @Test
+    public void loginValidUserFromText() throws IOException {
+        testCaseName = DataAdapter.readDataFromTextFile()[0];
+        username = DataAdapter.readDataFromTextFile()[1];
+        password = DataAdapter.readDataFromTextFile()[2];
+        DataAdapter.readDataFromTextFile();
+        landingPage = new LandingPage(driver);
+        loginForm = landingPage.openLoginForm();
+        personalPage = loginForm.loginValidUser(username, password);
+        Assert.assertEquals(personalPage.getWelcomeText(),"Welcome "+username);
+    }
+
+    @Test
+    public void loginValidUserFromJson() throws IOException {
+        testCaseName = DataAdapter.readDataFromJsonFile()[0];
+        username = DataAdapter.readDataFromTextFile()[1];
+        password = DataAdapter.readDataFromTextFile()[2];
+        DataAdapter.readDataFromTextFile();
+        landingPage = new LandingPage(driver);
+        loginForm = landingPage.openLoginForm();
+        personalPage = loginForm.loginValidUser(username, password);
+        Assert.assertEquals(personalPage.getWelcomeText(),"Welcome "+username);
+    }
+
+
+    @AfterMethod
+    public void afterMethod(){
+        landingPage = personalPage.clickLogoutLink();
     }
 
     @AfterTest
     public void tearDown(){
+
         driver.quit();
     }
 
